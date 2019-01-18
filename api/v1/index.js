26d7992fb796787ae610048c25f58700d9880d5d
afshanaman <afshan.aman@gmail.com>
2019-01-18 16:02:42 -0700

    Added welcome messages to the apis home routes

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 
-//**** API version 1 ****//
-router.use('/', require('./routes/admin'))
+//**** API version 1 Home ****//
+router.get('/', (req, res) => res.json("Welcome to API version 1"))
+
+//**** API version 1 Routes ****//
 router.use('/', require('./routes/user'))
+router.use('/', require('./routes/admin'))
 
 module.exports = router
--- api/v2/index.js
+++ api/v2/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 
-//**** API version 2 ****//
+//**** API version 2 Home ****//
+router.get('/', (req, res) => res.json("Welcome to API version 2. There are some awesome changes here!"))
+
+//**** API version 2 Routes ****//
 router.use('/', require('./routes/user'))
 
-
-//**** API version 1 ****//
+//**** API version 1 Routes ****//
 router.use('/', require('../v1/routes/admin'))
 
 module.exports = router
